78e700ae983a24d143ec53f4b2cf891279ae8e38
soupfoo <118446088+soupfoo@users.noreply.github.com>
2023-10-31 22:46:32 +0545

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/soupfoo/godc
+
+go 1.18
---
+++ godc.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Stack struct {
+	data []interface{}
+}
+
+func (s *Stack) isEmpty() bool {
+	return len(s.data) == 0
+}
+
+func (s *Stack) Push(val interface{}) {
+	s.data = append(s.data, val)
+}
+
+func (s *Stack) Pop() interface{} {
+	if s.isEmpty() {
+		return nil
+	}
+	lastIndex := len(s.data) - 1
+	lastData := s.data[lastIndex]
+	s.data = s.data[:lastIndex]
+	return lastData
+}
+
+func (s *Stack) Peek() interface{} {
+	if s.isEmpty() {
+		return nil
+	}
+	lastIndex := len(s.data) - 1
+	lastData := s.data[lastIndex]
+	return lastData
+}
+
+func (s *Stack) Reverse() {
+	for i, j := 0, len(s.data)-1; i < j; i, j = i+1, j-1 {
+		s.data[i], s.data[j] = s.data[j], s.data[i]
+	}
+}
+
+func showError(err error) {
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func main() {
+	dc := new(Stack)
+	reader := bufio.NewReader(os.Stdin)
+
+	for true {
+		input, _ := reader.ReadString('\n')
+		input = strings.TrimSpace(input)
+
+		if input == "" {
+			continue
+		}
+		switch input {
+		// Arithmetic operations
+		case "+":
+			x, y := dc.Pop().(float64), dc.Pop().(float64)
+			dc.Push(x + y)
+		case "-":
+			x, y := dc.Pop().(float64), dc.Pop().(float64)
+			dc.Push(y - x)
+		case "*":
+			x, y := dc.Pop().(float64), dc.Pop().(float64)
+			dc.Push(x * y)
+		case "/":
+			x, y := dc.Pop().(float64), dc.Pop().(float64)
+			dc.Push(y / x)
+		case "%":
+			x, y := dc.Pop().(float64), dc.Pop().(float64)
+			dc.Push(math.Mod(y, x))
+		case "~":
+			x, y := dc.Pop().(float64), dc.Pop().(float64)
+			dc.Push(y / x)
+			dc.Push(math.Mod(y, x))
+		case "^":
+			x, y := dc.Pop().(float64), dc.Pop().(float64)
+			dc.Push(math.Pow(y, x))
+		case "v":
+			x := dc.Pop().(float64)
+			dc.Push(math.Sqrt(x))
+
+			// Printing Commands
+		case "f":
+			fmt.Println(dc.data...)
+		case "p":
+			fmt.Println(dc.Peek())
+		case "n":
+			fmt.Println(dc.Pop())
+		case "q":
+			os.Exit(0)
+
+			// default case (push the number to the stack)
+		default:
+			num, err := strconv.ParseFloat(input, 64)
+			showError(err)
+			dc.Push(num)
+		}
+	}
+
+}
